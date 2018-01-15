a4d392851f8517a18db18443b3ff394abb5ff127
ManuHei <35437580+ManuHei@users.noreply.github.com>
2018-01-15 02:04:21 +0100

    Add files via upload
    enemy hinzugefügt

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float MovementSpeed;
+    public Rigidbody2D Player;
+    
+    
+    public Vector2 direction;
+    public float distance; //Abstand zwischen Enemy und Player
+    public float aggressionsDistance; //Abstand, ab dem Enemy eine Attacke startet
+    public float auszeit; //Mindestzeitraum zwischen zwei Angriffen
+    private float letzteAttacke; //Zeit der letzten Attacke
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        getDirection();
+	}
+
+
+    //Errechnet Richtung, in der der Spieler vom Enemy aus sich befindet
+    public void getDirection ()
+    {
+        float xPlayerPosition = Player.position.x;
+        float yPlayerPosition = Player.position.y;
+
+        direction = new Vector2(xPlayerPosition - this.transform.position.x, yPlayerPosition - this.transform.position.y);
+
+        distance = direction.magnitude;
+
+        //Vektor normieren
+         direction.Normalize();
+    }
+
+    
+
+    private void FixedUpdate()
+    {
+        if (distance < aggressionsDistance && auszeit < Time.time - letzteAttacke)
+        {
+            attack();
+            
+        }
+        else if (auszeit < Time.time - letzteAttacke)
+        {
+            move();
+        }
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    //Bewegt Enemy
+    private void move ()
+    {
+        transform.Translate(direction * MovementSpeed * Time.deltaTime);   
+    }
+
+    //Attacke
+    private void attack ()
+    {
+        Rigidbody2D rb;
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(direction*5, ForceMode2D.Impulse);
+        letzteAttacke = Time.time;
+        Debug.Log("Attacke   " + letzteAttacke);
+    }
+
+    
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -25,15 +25,54 @@ public class PlayerBehaviour : MonoBehaviour {
         Cam.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Cam.transform.position.z);
     }
 
-    private void FixedUpdate() {
-        move();
+    //Player wird von Trigger berührt
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            wirdWeggestoßen(collision);
+        }
     }
 
+    //Spieler wird weggestoßen
+    private void wirdWeggestoßen (Collider2D collision)
+    {
+        EnemyBehaviour enemy = collision.gameObject.GetComponent<EnemyBehaviour>();
+        Vector2 direction = enemy.direction;
+
+        Rigidbody2D rb;
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(direction*5, ForceMode2D.Impulse);
+    }
+
+
+
+
+
+    private void FixedUpdate() {
+        //move();
+        moveGleichschnell();
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    //Player bewegt sich diagonal schneller
     private void move() {
         float xMovement = horizontalInput * Time.deltaTime * MovementSpeed;
         float yMovement = verticalInput * Time.deltaTime * MovementSpeed;
 
         transform.Translate(new Vector3(xMovement, yMovement, transform.position.z));
+    }
+
+    //Player bewegt sich auch diagonal mit der gleichen Geschwindigkeit
+    private void moveGleichschnell()
+    {
+
+        //Richtungsvektor erstellen
+        Vector2 direction = new Vector2 (horizontalInput, verticalInput);
+        //Vektor normieren (auf Länge 1 bringen)
+        direction.Normalize();
+
+        transform.Translate(direction * Time.deltaTime * MovementSpeed);
     }
 
 }
